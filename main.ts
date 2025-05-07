dafe0aa5856e22ce0c08e060503d945e3e25d9e5
Taras Glek <taras@glek.net>
2025-05-07 12:44:09 +0300

    misc

--- main.ts
+++ main.ts
@@ -1,49 +1,65 @@
-
-const DOWNLOAD_PREFIX = 'https://cdn.jsdelivr.net/npm/@duckdb/duckdb-wasm/dist/';
-import fs from 'fs/promises';
-import path from 'path';
-import { fileURLToPath } from 'node:url';
+import fs from "fs/promises";
+import path from "path";
+import { fileURLToPath } from "url";
 
 const MANUAL_BUNDLES = {
-    mvp: {
-        mainModule: "duckdb-mvp.wasm",
-        mainWorker: "duckdb-node-blocking.cjs",
-    },
+  mvp: {
+    mainModule: "duckdb-mvp.wasm",
+    mainWorker: "duckdb-node-blocking.cjs",
+  },
 };
-const currentModuleDir = path.dirname(fileURLToPath(import.meta.url));
 
-async function ensureFileDownloaded(fileName: string, downloadUrlPrefix: string, targetDir: string) {
-    const filePath = path.join(targetDir, fileName);
-    try {
-        await fs.access(filePath);
-    } catch (error) {
-        console.log(`${fileName} not found. Downloading from ${downloadUrlPrefix}${fileName}...`);
-        const response = await fetch(`${downloadUrlPrefix}${fileName}`);
-        if (!response.ok || !response.body) {
-            throw new Error(`Failed to download ${fileName}: ${response.statusText}`);
-        }
-        const fileBuffer = await response.arrayBuffer();
-        await fs.writeFile(filePath, Buffer.from(fileBuffer));
-        console.log(`${fileName} downloaded successfully to ${filePath}.`);
+async function ensureFileDownloaded(fileName: string) {
+  const targetDir = path.dirname(fileURLToPath(import.meta.url));
+  const DOWNLOAD_PREFIX =
+    "https://cdn.jsdelivr.net/npm/@duckdb/duckdb-wasm/dist/";
+  const filePath = path.join(targetDir, fileName);
+  try {
+    await fs.access(filePath);
+  } catch (error) {
+    console.log(
+      `${fileName} not found. Downloading from ${DOWNLOAD_PREFIX}${fileName}...`,
+    );
+    const response = await fetch(`${DOWNLOAD_PREFIX}${fileName}`);
+    if (!response.ok || !response.body) {
+      throw new Error(`Failed to download ${fileName}: ${response.statusText}`);
     }
+    const fileBuffer = await response.arrayBuffer();
+    await fs.writeFile(filePath, Buffer.from(fileBuffer));
+    console.log(`${fileName} downloaded successfully to ${filePath}.`);
+  }
 }
 
 const wasmFileName = MANUAL_BUNDLES.mvp.mainModule;
 const workerFileName = MANUAL_BUNDLES.mvp.mainWorker;
 
-await ensureFileDownloaded(wasmFileName, DOWNLOAD_PREFIX, currentModuleDir);
-await ensureFileDownloaded(workerFileName, DOWNLOAD_PREFIX, currentModuleDir);
+await ensureFileDownloaded(wasmFileName);
+await ensureFileDownloaded(workerFileName);
 
-const workerModule = await import('./' + workerFileName);
+const workerModule = await import("./" + workerFileName);
 const worker_blocking = workerModule.default;
 
 const logger = new worker_blocking.ConsoleLogger();
-const ddb = await worker_blocking.createDuckDB(MANUAL_BUNDLES, logger, worker_blocking.NODE_RUNTIME);
+const ddb = await worker_blocking.createDuckDB(
+  MANUAL_BUNDLES,
+  logger,
+  worker_blocking.NODE_RUNTIME,
+);
 
-const absoluteWasmPath = path.join(currentModuleDir, MANUAL_BUNDLES.mvp.mainModule);
-const absoluteWorkerPath = path.join(currentModuleDir, MANUAL_BUNDLES.mvp.mainWorker);
-const inst = await ddb.instantiate(absoluteWasmPath, absoluteWorkerPath);
+const inst = await ddb.instantiate(
+  MANUAL_BUNDLES.mvp.mainModule,
+  MANUAL_BUNDLES.mvp.mainWorker,
+);
 const db = await ddb.connect();
 const mainTsAbsolutePath = fileURLToPath(import.meta.url);
-ddb.registerFileURL(`main.ts`, mainTsAbsolutePath, worker_blocking.DuckDBDataProtocol.NODE_FS, false);
-console.log("inst", await db.query("select md5(content) as md5 from read_text('main.ts')").toArray()[0].md5);
+ddb.registerFileURL(
+  `main.ts`,
+  mainTsAbsolutePath,
+  worker_blocking.DuckDBDataProtocol.NODE_FS,
+  false,
+);
+console.log(
+  "inst",
+  await db.query("select md5(content) as md5 from read_text('main.ts')")
+    .toArray()[0].md5,
+);
