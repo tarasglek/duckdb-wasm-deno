fa109e1981b1fb9b0ed5375e87d5ad11dac2b04a
Taras Glek <taras@glek.net>
2025-05-07 12:16:06 +0300

    kinda works

--- main.ts
+++ main.ts
@@ -1,23 +1,26 @@
 import * as duckdb from '@duckdb/duckdb-wasm';
-import duckdb_wasm from '@duckdb/duckdb-wasm/dist/duckdb-mvp.wasm?url';
-import mvp_worker from '@duckdb/duckdb-wasm/dist/duckdb-browser-mvp.worker.js?url';
-import duckdb_wasm_eh from '@duckdb/duckdb-wasm/dist/duckdb-eh.wasm?url';
-import eh_worker from '@duckdb/duckdb-wasm/dist/duckdb-browser-eh.worker.js?url';
+import worker_blocking from './duckdb-node-blocking.cjs';
+// import duckdb_wasm from '@duckdb/duckdb-wasm/dist/duckdb-mvp.wasm?url';
+// import mvp_worker from '@duckdb/duckdb-wasm/dist/duckdb-browser-mvp.worker.js?url';
+// import duckdb_wasm_eh from '@duckdb/duckdb-wasm/dist/duckdb-eh.wasm?url';
+// import eh_worker from '@duckdb/duckdb-wasm/dist/duckdb-browser-eh.worker.js?url';
 
 const MANUAL_BUNDLES = {
     mvp: {
-        mainModule: duckdb_wasm,
-        mainWorker: mvp_worker,
-    },
-    eh: {
-        mainModule: duckdb_wasm_eh,
-        mainWorker: eh_worker,
+        mainModule: "duckdb-mvp.wasm",
+        mainWorker: "duckdb-node-blocking.cjs",
     },
 };
 // Select a bundle based on browser checks
 const bundle = await duckdb.selectBundle(MANUAL_BUNDLES);
 // Instantiate the asynchronus version of DuckDB-wasm
-const worker = new Worker(bundle.mainWorker!);
-const logger = new duckdb.ConsoleLogger();
-const db = new duckdb.AsyncDuckDB(logger, worker);
-await db.instantiate(bundle.mainModule, bundle.pthreadWorker);
+// const worker = new worker_blocking.Worker(bundle.mainWorker!);
+// const worker = new worker_blocking.create(bundle.mainWorker!);
+// console.log(worker_blocking.DEFAULT_RUNTIME);
+// console.log(duckdb);
+const logger = new duckdb.ConsoleLogger;
+// console.log("worker_blocking", worker_blocking.NODE_RUNTIME)
+const ddb = await worker_blocking.createDuckDB(MANUAL_BUNDLES, logger, worker_blocking.NODE_RUNTIME);
+const inst = await ddb.instantiate(bundle.mainModule, bundle.mainWorker);
+const db = await ddb.connect();
+console.log("inst", await db.query("SELECT 1").toArray());
